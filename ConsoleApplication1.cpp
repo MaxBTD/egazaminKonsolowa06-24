08a50649f9768214028363220df4468226958850
5m <konradek982@gmail.com>
2024-11-22 13:06:51 +0100

    zadanie konsolowe c# i c++

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+using namespace std;
+
+int zliczaniePunktow(int liczbaKoste, int wylosLiczb[]);
+
+int main()
+{
+    string dalejGrac = "t";
+    srand(time(0));
+    int liczbaKostek = 0;
+
+    while (liczbaKostek < 3 || liczbaKostek > 10) {
+        cout << "Ile kostek chcesz rzucic?(3 - 10)\n";
+        cin >> liczbaKostek;
+    }
+
+    while (dalejGrac == "t") {
+        dalejGrac = "";
+        int wylosLiczb[10];
+
+        for (int i = 0; i < liczbaKostek; i++) {
+            int rzutKostka = 0;
+            while (rzutKostka < 1 || rzutKostka > 6) {
+                rzutKostka = rand();
+            }
+            wylosLiczb[i] = rzutKostka;
+            cout << "Kostka nr " << i + 1 << ": " << rzutKostka << "\n";
+        }
+
+        cout << "Liczba uzyskanych punktow: " << zliczaniePunktow(liczbaKostek, wylosLiczb) << "\n";
+        while (dalejGrac != "t" && dalejGrac != "n") {
+            cout << "Jeszcze raz? (t/n) \n";
+            cin >> dalejGrac;
+        }
+    }
+}
+
+int zliczaniePunktow(int liczbaKostek, int wylosLiczb[]) {
+    int wynik = 0;
+    for (int i = 0; i < liczbaKostek; i++) {
+        int sprawLiczb = wylosLiczb[i];
+        int ileRazy = 1;
+        if (i < 10) {
+            for (int j = i + 1; j < liczbaKostek; j++) {
+                if (sprawLiczb == wylosLiczb[j]) {
+                    ileRazy++;
+                    wylosLiczb[j] = 0;
+                }
+            }
+            if (ileRazy > 1)
+                wynik += sprawLiczb * ileRazy;
+        }
+    }
+
+    return wynik;
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿int zliczaniePunktow(int liczbaKostek, int[] wylosLiczb){
+    int wynik = 0;
+    for(int i = 0; i < liczbaKostek; i++)
+    {
+        int sprawLiczb = wylosLiczb[i];
+        int ileRazy = 1;
+        if (i < 10)
+        {
+            for(int j = i + 1; j < liczbaKostek; j++)
+            {
+                if(sprawLiczb == wylosLiczb[j])
+                {
+                    ileRazy++;
+                    wylosLiczb[j] = 0;
+                }
+            }
+            if (ileRazy > 1)
+                wynik += sprawLiczb * ileRazy;
+        }
+    }
+
+    return wynik;
+}
+
+string dalejGrac = "t";
+Random random = new Random();
+int liczbaKostek = 0;
+
+while(liczbaKostek < 3 || liczbaKostek > 10)
+{
+    Console.WriteLine("Ile kostek chcerz rzucic?(3 - 10)");
+    int.TryParse(Console.ReadLine(), out liczbaKostek);
+}
+
+while(dalejGrac == "t")
+{
+    dalejGrac = "";
+    int[] wylosLiczb = new int[liczbaKostek];
+
+    for(int i = 0; i < liczbaKostek; i++)
+    {
+        wylosLiczb[i] = random.Next(1, 7);
+        Console.WriteLine($"Kostka nr {i + 1}: {wylosLiczb[i]}\n");
+    }
+    Console.WriteLine($"Liczba uzyskanych punktow: {zliczaniePunktow(liczbaKostek, wylosLiczb)}");
+    while (dalejGrac != "t" && dalejGrac != "n")
+    {
+        Console.WriteLine("Jeszcze raz? (t/n) \n");
+        dalejGrac = Console.ReadLine();
+    }
+}
